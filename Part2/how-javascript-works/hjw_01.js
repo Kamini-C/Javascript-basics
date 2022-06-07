2e96ff715d652e5fc0905047cbf1b38d306ec0b6
Kamini-C <iamkaminichhabra@gmail.com>
2022-06-07 22:30:43 +0530

    how js works

---
+++ Part2/how-javascript-works/hjw_01.js
@@ -0,0 +1,20 @@
+// compilation 
+// code execution
+
+// why compilation 
+
+// How javascript code executes 
+
+
+// what is global exection context ? 
+// what is local execution context ? 
+// closures
+console.log(this);
+console.log(window);
+console.log(firstName);
+var firstName = "Kamini";
+// var --> undefined
+// let --> ReferenceError cannot access 'firstname'
+// const --> ReferenceError cannot access 'firstname'
+
+console.log(firstName);
